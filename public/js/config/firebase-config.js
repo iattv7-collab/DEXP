908ec498cdfd97bea55f84d633b57b27c1f2ed10
iattv7 <iattv7@users.noreply.github.com>
2026-05-12 21:47:22 -0400

    Add Firebase web configuration

---
+++ public/js/config/firebase-config.js
@@ -0,0 +1,10 @@
+// public/js/config/firebase-config.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyClcn2PQKYMnKkGwG5xX3WCNYpexPsY-mI",
+  authDomain: "dexp-5056c.firebaseapp.com",
+  projectId: "dexp-5056c",
+  storageBucket: "dexp-5056c.firebasestorage.app",
+  messagingSenderId: "924644421556",
+  appId: "1:924644421556:web:268e79b673f94a2d34cce1"
+};
